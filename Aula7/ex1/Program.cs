af81ce54c2d8c21190823beddb805c23789081b1
sabslf <lfsabrina7@gmail.com>
2025-06-02 11:11:07 -0300

    ClonagemRepositorio

---
+++ Aula7/ex1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main()
+    {
+       int[] numeros = new int[10];
+       int leitura
+       for (leitura = 0; leitura < 10; leitura++){
+        Console.Write($"Digite um número inteiro:");
+       }
---
+++ ClonagemRepositorio/Ex3ClonagemGit.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "pt_BR.utf8"); //Para usar acentuação
+    cout << "\033[1;34m***************************************************\033[0m\n";
+    cout << "\033[1;32m*                                                 *\033[0m\n";
+    cout << "\033[1;32m*   \033[1;33m TURMA 34DS!                     \033[1;32m          *\033[0m\n";
+    cout << "\033[1;32m*                                                 *\033[0m\n";
+    cout << "\033[1;32m*   \033[1;36mVocê clonou um repositório público do Git     \033[1;32m*\033[0m\n";
+    cout << "\033[1;32m*   \033[1;36mEBA, funcionou!                 \033[1;32m*\033[0m\n";
+    cout << "\033[1;32m*                                                 *\033[0m\n";
+    cout << "\033[1;34m***************************************************\033[0m\n";
+
+    return 0;
+}
